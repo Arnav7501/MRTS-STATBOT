53cad10167632fb1f1251710113202f76e6ac653
Luckanio <lalsin.nilian2025@gmail.com>
2023-06-10 01:25:15 -0500

    fixed pastebin formatting  and issue with the nested for loop

--- patchnotes.py
+++ patchnotes.py
@@ -2,14 +2,21 @@ from slpp import slpp as lua
 import requests
 
 def TableToDict(pastebinURL):
-    table = "{" + requests.get(pastebinURL).text[11:4127] + requests.get(pastebinURL).text[4157:-15] + "}"
+    table = "{" + requests.get(pastebinURL).text[17:4133] + requests.get(pastebinURL).text[4163:-14] + "}"
+    table.replace("v1.", "")
     #goofy numbers since first and last 2 lines in pastebin are not relevant and same with 244-246
     return lua.decode(table)
 
 def PrintChanges(old, updated):
     changes = ""
-    for x in old:
-        for y in x:
-            if(old[x][y] != updated[x][y]):
-                changes += f"{x} {y} changed from {old[x][y]} to {updated[x][y]}\n"
+    for thing in old:
+        for attribute in old[thing]:
+            try:
+              if old[thing][attribute] != updated[thing][attribute]:
+                  changes += f"{thing}'s {attribute} changed from {old[thing][attribute]} to {updated[thing][attribute]}\n"
+            except KeyError as e:
+                print(f"KeyError: {e}")
+                print(f"x: {thing}, y: {attribute}")
+                print(f"Keys in old[x]: {old[thing].keys() if thing in old else 'x not in old'}")
+                print(f"Keys in updated[x]: {updated[thing].keys() if thing in updated else 'x not in updated'}")
     return changes

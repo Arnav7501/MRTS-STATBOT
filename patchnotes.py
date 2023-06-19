bdbb39d8831be962171b2975415126ef5bc063fa
Luckanio <lalsin.nilian2025@gmail.com>
2023-06-19 17:53:43 -0500

    fixed some more stuff

--- patchnotes.py
+++ patchnotes.py
@@ -11,8 +11,6 @@ def TableToDict(pastebinURL):
     table = table.replace("v1.", "")
     table = table.replace("v1", "")
     table = table.replace("\r\n\t--// Buildings //--\r\n\r\n", "")
-
-    # goofy numbers since first and last 2 lines in pastebin are not relevant and same with 244-246
     return lua.decode(table)
 
 
@@ -24,6 +22,8 @@ def PrintChanges(old, updated):
                 changes += checkAttribute(old, updated, thing, attribute)
         else:
             changes += f"* {thing} removed\n"
+    if len(changes) > 0:
+        changes = "```" + changes + "```"
     return changes
 
 
@@ -31,11 +31,16 @@ def checkAttribute(old, updated, thing, attribute):
     changes = ""
     if attribute in updated[thing]:
         if old[thing][attribute] != updated[thing][attribute]:
-            changes += f"{isGood(attribute, old[thing][attribute], new[thing][attribute])} {thing}'s {attribute} changed from {old[thing][attribute]} to {updated[thing][attribute]}\n"
+            sign = isGood(attribute, old[thing][attribute], updated[thing][attribute])
+            signWord = "changed"
+            match sign:
+                case "+":
+                    signWord = "increased"
+                case "-":
+                    signWord = "decreased"
+            changes += f"{sign} {thing}'s {attribute} {signWord} from {old[thing][attribute]} to {updated[thing][attribute]}\n"
     else:
         changes += f"* {attribute} from {thing} removed"
-    if len(changes) > 0:
-        changes = "```" + changes + "```"
     return changes
 
 def isGood(attribute, old, new):

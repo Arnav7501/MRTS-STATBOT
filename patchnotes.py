56cbddd84c1f91d9b2e7048092e1aa40167d435f
Luckanio <lalsin.nilian2025@gmail.com>
2023-06-19 10:55:02 -0500

    made the code and the patch notes look better hope it doesnt break anything

--- patchnotes.py
+++ patchnotes.py
@@ -1,6 +1,8 @@
 from slpp import slpp as lua
 import requests
 
+inverse = {"Damage", "MaxHealth", "Range", "Speed", "SplashRange", "AddHealth", "AddBuildings", "GiveCash", "AddUnits"}
+verse = {"Cost", "Space", "Time", "Rate"}
 
 def TableToDict(pastebinURL):
     table = "{" + requests.get(pastebinURL).text[18:-14] + "}"
@@ -17,19 +19,38 @@ def PrintChanges(old, updated):
     for thing in old:
         if thing in updated:
             for attribute in old[thing]:
-                if attribute in updated[thing]
-                    try:
-                        if old[thing][attribute] != updated[thing][attribute]:
-                            changes += f"{thing}'s {attribute} changed from {old[thing][attribute]} to {updated[thing][attribute]}\n"
-                    except KeyError as e:
-                        print(f"KeyError: {e}")
-                        print(f"x: {thing}, y: {attribute}")
-                        print(
-                            f"Keys in old[x]: {old[thing].keys() if thing in old else 'x not in old'}")
-                        print(
-                            f"Keys in updated[x]: {updated[thing].keys() if thing in updated else 'x not in updated'}")
-                else:
-                    changes += f"{attribute} from {thing} removed"
+                checkAttribute(old, updated, thing, attribute)
         else:
-            changes += f"{thing} removed\n"
+            changes += f"* {thing} removed\n"
     return changes
+
+def checkAttribute(old, updated, thing, attribute):
+    changes = ""
+        if attribute in updated[thing]
+            try:
+                if old[thing][attribute] != updated[thing][attribute]:
+                    match checkInverse(attribute):
+                        case 1:
+                            changes += f"+ {thing}'s {attribute} changed from {old[thing][attribute]} to {updated[thing][attribute]}\n"
+                        case -1:
+                            changes += f"- {thing}'s {attribute} changed from {old[thing][attribute]} to {updated[thing][attribute]}\n"
+                        case 0:
+                            changes += f"\* {thing}'s {attribute} changed from {old[thing][attribute]} to {updated[thing][attribute]}"
+
+                
+            except KeyError as e:
+                print(f"KeyError: {e}")
+                print(f"x: {thing}, y: {attribute}")
+                print(f"Keys in old[x]: {old[thing].keys() if thing in old else 'x not in old'}")
+                print(f"Keys in updated[x]: {updated[thing].keys() if thing in updated else 'x not in updated'}")
+        else:
+            changes += f"* {attribute} from {thing} removed"
+
+
+def checkInverse(attribute):
+    if attribute in inverse:
+        return 1
+    elif attribute in verse:
+        return -1
+    else:
+        return 0

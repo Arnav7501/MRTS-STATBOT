74175d66db80eabecb16863d41991abb028542ad
Luckanio <lalsin.nilian2025@gmail.com>
2023-06-19 09:49:12 -0500

    i dont think this'll be needed but you never know with dmnks

--- patchnotes.py
+++ patchnotes.py
@@ -17,16 +17,19 @@ def PrintChanges(old, updated):
     for thing in old:
         if thing in updated:
             for attribute in old[thing]:
-                try:
-                    if old[thing][attribute] != updated[thing][attribute]:
-                        changes += f"{thing}'s {attribute} changed from {old[thing][attribute]} to {updated[thing][attribute]}\n"
-                except KeyError as e:
-                    print(f"KeyError: {e}")
-                    print(f"x: {thing}, y: {attribute}")
-                    print(
-                        f"Keys in old[x]: {old[thing].keys() if thing in old else 'x not in old'}")
-                    print(
-                        f"Keys in updated[x]: {updated[thing].keys() if thing in updated else 'x not in updated'}")
+                if attribute in updated[thing]
+                    try:
+                        if old[thing][attribute] != updated[thing][attribute]:
+                            changes += f"{thing}'s {attribute} changed from {old[thing][attribute]} to {updated[thing][attribute]}\n"
+                    except KeyError as e:
+                        print(f"KeyError: {e}")
+                        print(f"x: {thing}, y: {attribute}")
+                        print(
+                            f"Keys in old[x]: {old[thing].keys() if thing in old else 'x not in old'}")
+                        print(
+                            f"Keys in updated[x]: {updated[thing].keys() if thing in updated else 'x not in updated'}")
+                else:
+                    changes += f"{attribute} from {thing} removed"
         else:
             changes += f"{thing} removed\n"
     return changes
